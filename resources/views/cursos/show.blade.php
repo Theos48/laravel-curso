ba8ab7355570cf563089d627b33079739e007ce9
David Ramirez <David_Ramirezz@hotmail.com>
2022-05-20 11:11:39 -0500

    Listar y leer registros

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.plantilla')
 
-@section('title', 'Curso ' . $curso)
+@section('title', 'Curso ' . $curso->id)
 
 @section('content')
-    <h1>En esta paginan muestra el curso: {{$curso}}</h1>
+    <h1>En esta paginan muestra el curso: {{$curso->name}}</h1>
+    <a href="{{route('cursos.index')}}">Volver a cursos</a>
+    <p>{{$curso->descripcion}}</p>
 @endsection
