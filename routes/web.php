7fca7247ba61f53c0654d3f3d117f92286ea5f6c
David Ramirez <David_Ramirezz@hotmail.com>
2022-05-20 09:26:50 -0500

    Rutas y controladores

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,21 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
 
-Route::get('cursos', function () {
-    return "Vista cursos";
-});
+Route::get('cursos', [CursoController::class, 'index']);
 
-Route::get('cursos/create', function () {
-    return 'Estas en seccion de creacion de cursos';
-});
+Route::get('cursos/create', [CursoController::class, 'create']);
 
-Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
-    if ($categoria) {
-        return "Bienviendo al curso $curso de la categoria $categoria";
-    }
-    return  "Bienviendo al curso $curso";
-});
+Route::get('cursos/{curso}', [CursoController::class, 'show']);
+
+// Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+//     if ($categoria) {
+//         return "Bienviendo al curso $curso de la categoria $categoria";
+//     }
+//     return  "Bienviendo al curso $curso";
+// });
